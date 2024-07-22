319904fba34c9e338eca3fc7b255ac29893b458c
RodrigoFontesCardoso <rodrigofontescardoso0@gmail.com>
2024-07-22 17:09:28 -0300

    change background based on media queries

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagen light
     img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute("alt", "uhihuihuguuygu")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo,usando oculos e camisa preta, barba e fundo amarelo"
+    )
   } else {
     // se tiver sem light mode, mander a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute(
       "alt",
-      "Foto de Mayk Brito sorrindo,usando oculos e camisa preta, barba e fundo amarelo"
+      "Foto de Mayk Brito sorrindo,usando oculos de sol, jaqueta preta e camisa preta, sem barba e fundo azul "
     )
   }
 }
+
+
